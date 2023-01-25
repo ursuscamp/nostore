6d9235d8c023eb1ac2d33e63c467e40514c63b7c
Ryan Breen <ryan@breen.xyz>
2023-01-24 23:17:15 -0500

    Validation for relay URL entry.

--- Shared (Extension)/Resources/options.js
+++ Shared (Extension)/Resources/options.js
@@ -6,6 +6,7 @@ Alpine.data('options', () => ({
     profileIndex: 0,
     relays: [],
     newRelay: '',
+    urlError: '',
 
     async init() {
         await this.getProfileNames();
@@ -35,16 +36,37 @@ Alpine.data('options', () => ({
             payload: [this.profileIndex, this.relays],
         });
         await this.getRelaysForProfile();
+        this.newRelay = '';
     },
 
     async addRelay() {
-        this.relays.push({ url: this.newRelay, read: true, write: true });
-        await this.saveRelaysForProfile();
+        try {
+            let url = new URL(this.newRelay);
+            if (url.protocol !== 'wss:') {
+                this.setUrlError('Must be a websocket url');
+            }
+            let urls = this.relays.map(v => v.url);
+            if (urls.includes(url.href)) {
+                this.setUrlError('URL already exists');
+                return;
+            }
+            this.relays.push({ url: url.href, read: true, write: true });
+            await this.saveRelaysForProfile();
+        } catch (error) {
+            this.setUrlError('Invalid websocket URL');
+        }
     },
 
     async deleteRelay(index) {
         this.relays.splice(index, 1);
         await this.saveRelaysForProfile();
+    },
+
+    setUrlError(message) {
+        this.urlError = message;
+        setTimeout(() => {
+            this.urlError = '';
+        }, 3000);
     },
 }));
 
